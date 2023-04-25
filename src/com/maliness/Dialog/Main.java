bd5d0ec4f0f163356138493d3760a95e1c1dc648
Niktos308 <nikita.tos.veligotsky@gmail.com>
2023-04-25 18:37:12 +0200

    learn how to use if/else and тернарный оператор)))))) dont kill me in coding(((((

--- src/com/maliness/Dialog/Main.java
+++ src/com/maliness/Dialog/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       day1(); //run code from method (block) with name "day1", change name for other
+        //day1();
+        //day2();//run code from method (block) with name "day1", change name for other
+        //day3();
+        day3_1();
     }
 
     public static void day1() { //copy and change name "day1" and code in {}
@@ -20,5 +23,57 @@ public class Main {
         System.out.printf("Name: %s Age: %d Height: %.2f \n", name , age, height);
         in.close();
     }
-}
 
+    public static void day2() {
+        Scanner cool = new Scanner(System.in);
+        System.out.print("Input name, please: ");
+        String name = cool.nextLine();
+        System.out.print("Input age: ");
+        int age = cool.nextInt();
+        System.out.print("Input height: ");
+        double height = cool.nextDouble();
+        System.out.print("Can he swim: ");
+        boolean swim = cool.nextBoolean();
+        System.out.printf("Name: %s Age: %d Height: %.2f Can he swim: %b", name, age, height, swim);
+    }
+
+    public static void day3() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        System.out.print("Input age: ");
+        int age = in.nextInt();
+        System.out.print("Input height: ");
+        double height = in.nextDouble();
+        System.out.print("Can he swim: ");
+        boolean canHeSwim = in.nextBoolean();
+        String answer;
+        if (canHeSwim) {
+            answer = "Yes";
+        } else {
+            answer = "No";
+        }
+        System.out.printf("Name: %s Age: %d Height: %.2f Can he swim: %s", name, age, height, answer);
+        in.close();
+    }
+
+    public static void day3_1() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        System.out.print("Input age: ");
+        int age = in.nextInt();
+        System.out.print("Input height: ");
+        double height = in.nextDouble();
+        System.out.print("Can he swim: ");
+        boolean canHeSwim = in.nextBoolean();
+        String answer = canHeSwim ? "Yes" : "No"; //тернарный оператор
+//        if (canHeSwim) {
+//            answer = "Yes";
+//        } else {
+//            answer = "No";
+//        }
+        System.out.printf("Name: %s Age: %d Height: %.2f Can he swim: %s", name, age, height, answer);
+        in.close();
+    }
+}
